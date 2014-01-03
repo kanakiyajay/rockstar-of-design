9f2703b49fa9833400f6efb7d272fc1f9efd54b9
Jay Kanakiya <kanakiyajay@yahoo.com>
2014-01-03 19:17:19 +0530

    Angular domainlist

--- js/app.js
+++ js/app.js
@@ -6,3 +6,24 @@
 **/
 
 "use strict";
+
+var App = angular.module("rockstar",[]);
+
+App.controller("RckCtrl",function  ($scope) {
+
+	/* Will persist via LocalStorage */
+	$scope.cartDomains = [];
+
+	$scope.availableDomains = [];
+
+	$scope.search = function  () {
+		var dText = $scope.searchText;
+		$scope.availableDomains = [];
+		$scope.availableDomains.splice(0,0,dText+".net",dText+".in",dText+".com",dText+".org");
+	};
+
+	$scope.addToCart = function  (i) {
+		$scope.cartDomains.push($scope.availableDomains[i]);
+	};
+
+});
